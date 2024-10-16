fae4f57d67940a6ec5e9ac7b3c7f8317eba67997
Alok Rajbongshi <alokraj00061@.com>
2024-10-16 14:41:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Components/Navbar/Navbar"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+
+        <h1 className='text-7xl'>Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
