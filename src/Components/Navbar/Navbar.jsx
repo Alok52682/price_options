a66f7152a3d1287ee7e5719ce1b2c1a42c5165c0
Alok Rajbongshi <alokraj00061@.com>
2024-10-16 21:16:43 +0600

    navbar completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Link from "../Link/Link";
+import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
 
 const Navbar = () => {
 
@@ -12,13 +14,22 @@ const Navbar = () => {
     ];
 
     return (
-        <nav className="">
-            {
-                routes.map(route => <li key={route.id}>
-                    <a href={route.path}>{route.name}</a></li>
-                )
-            }
+        <nav className="text-black p-6 bg-yellow-200">
+            <div className=" md:hidden text-2xl" onClick={() => setOpen(!open)}>
+                {
+                    open === true ?
+                        <AiOutlineMenuFold />
+                        : <AiOutlineMenuUnfold />
+                }
 
+            </div>
+            <ul className={`md:flex duration-1000 absolute md:static
+            ${open ? 'top-16' : '-top-60'}
+            bg-yellow-200 px-6`}>
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
         </nav>
     );
 };
