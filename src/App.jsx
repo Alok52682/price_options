6cc172448d5ecc442de8abacf140cd95eddf980e
Alok Rajbongshi <alokraj00061@.com>
2024-10-16 21:56:52 +0600

    price options components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./Components/Navbar/Navbar"
+import PriceOptions from "./Components/PriceOptions/PriceOptions"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
         <Navbar></Navbar>
 
         <h1 className='text-7xl'>Vite + React</h1>
+        <PriceOptions></PriceOptions>
       </div>
     </>
   )
